ef0b9e2ef4a8f1311c0c1cdb5f7818963d34e474
nathanieleh <nathanieleh@gmail.com>
2023-09-05 15:39:24 -0700

    testing the video playback on mobile

--- src/script.js
+++ src/script.js
@@ -38,6 +38,11 @@ const observer = new IntersectionObserver((entries) => {
             if(entry.target.innerHTML.includes("Snake AI")){
                 snakeVideo1.setAttribute("src", "../img/SnakeAIBeginner.mp4");
                 snakeVideo2.setAttribute("src", "../img/SnakeAIExpert.mp4");
+                snakeVideo1.play();
+                snakeVideo2.play();
+                threeDAIVideo1.pause();
+                threeDAIVideo2.pause();
+                minesweeperVideo.pause();
                 minesweeperVideo.removeAttribute("src");
                 threeDAIVideo1.removeAttribute("src");
                 threeDAIVideo2.removeAttribute("src");
@@ -45,12 +50,22 @@ const observer = new IntersectionObserver((entries) => {
             else if(entry.target.innerHTML.includes("3D AI")){
                 threeDAIVideo1.setAttribute("src", "../img/3DAIBeginner.mp4");
                 threeDAIVideo2.setAttribute("src", "../img/3DAIExpert.mp4");
+                threeDAIVideo1.play();
+                threeDAIVideo2.play();
+                snakeVideo1.pause();
+                snakeVideo2.pause();
+                minesweeperVideo.pause();
                 minesweeperVideo.removeAttribute("src");
                 snakeVideo1.removeAttribute("src");
                 snakeVideo2.removeAttribute("src");
             }
-            else{
+            else if(entry.target.innerHTML.includes("Minesweeper")){
                 minesweeperVideo.setAttribute("src", "../img/MinesweeperDemo.mp4");
+                minesweeperVideo.play();
+                threeDAIVideo1.pause();
+                threeDAIVideo2.pause();
+                snakeVideo1.pause();
+                snakeVideo2.pause();
                 snakeVideo1.removeAttribute("src");
                 snakeVideo2.removeAttribute("src");
                 threeDAIVideo1.removeAttribute("src");
